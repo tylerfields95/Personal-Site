9b161464467ab023615e9a2b12b1f5b16bdc0d9d
Tyler Fields <tylerfields95@gmail.com>
2025-08-27 22:36:03 -0400

    * add header for navigation when in portrait layout

--- apps/ng-personal-site/src/app/components/header/header.ts
+++ apps/ng-personal-site/src/app/components/header/header.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, Signal, signal } from '@angular/core';
+import { Router, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
+  readonly isExpanded = signal(false);
+  readonly isHomeIconHovered = signal(false);
+  readonly widthClass = computed(() =>
+    this.isExpanded() ? 'h-64 items-end' : 'h-12 items-center'
+  );
+  public routes: Signal<Routes>;
 
+  constructor(private router: Router) {
+    this.routes = signal(this.router.config);
+    console.log(this.router.config);
+  }
+
+  toggleSidebar(): void {
+    this.onLeaveHomeIcon();
+    this.isExpanded.update((v) => !v);
+  }
+
+  goHome(): void {
+    this.router.navigate(['/']);
+  }
+
+  navigateToPath(path?: string): void {
+    this.router.navigate([`/${path}`]);
+  }
+
+  onEnterHomeIcon(): void {
+    this.isHomeIconHovered.set(true);
+  }
+
+  onLeaveHomeIcon(): void {
+    this.isHomeIconHovered.set(false);
+  }
 }
